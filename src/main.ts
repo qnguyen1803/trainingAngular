190730d8df7f1987a7b59228d0063ca80b27469c
qhnguyen <quanghuy.18031992@gmail.com>
2019-01-21 11:20:56 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  let hhaha: boolean;
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
