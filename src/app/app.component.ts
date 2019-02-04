56c7208ab75c1371402f8f52e4c32ea10c0b77f3
qhnguyen <quanghuy.18031992@gmail.com>
2019-02-04 17:17:20 +0100

    feat : add new service myService
    - Generate new service myService
    
    - Add new function doSomething to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyServiceService } from './my-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MyServiceService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'trainingAngular';
   realTime: number = Date.now();
+  private data: string[];
+
+  constructor(private myServiceService: MyServiceService) {
+
+  }
+
+  ngOnInit(): void { this.data = this.myServiceService.doSomething(); }
 }
